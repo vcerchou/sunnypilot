efe8e4f58efb3ae83fc46830d1ef15077f3f9d56
Jason Wen <haibin.wen3@gmail.com>
2023-06-07 17:32:53 -0400

    sunnypilot v2023.06.07 version: sunnypilot v0.9.2 release date: 2023-06-07T17:33:55 master commit: 01eb087b1164665bc373b8a87d29e00cd5f805de
    Signed-off-by: Jason Wen <haibin.wen3@gmail.com>

---
+++ body/board/obj/gitversion.h
@@ -0,0 +1,1 @@
+const uint8_t gitversion[8] = "b6af492a";
---
+++ system/fleetmanager/fleet_manager.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+import os
+import random
+import secrets
+import system.fleetmanager.helpers as fleet
+from flask import Flask, render_template, Response, request, send_from_directory, session, redirect, url_for
+from functools import wraps
+from system.loggerd.config import ROOT as REALDATA
+
+app = Flask(__name__)
+
+
+def login_required(f):
+  @wraps(f)
+  def decorated_route(*args, **kwargs):
+    if not session.get("logged_in"):
+      return redirect(url_for("index_page"))
+    return f(*args, **kwargs)
+  return decorated_route
+
+
+@app.route("/")
+def index_page():
+  if session.get("logged_in"):
+    return redirect(url_for("home_page"))
+  return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+  inputted_pin = request.form.get("pin")
+  with open(fleet.PIN_PATH + "otp.conf", "r") as file:
+    correct_pin = file.read().strip()
+
+  if inputted_pin == correct_pin:
+    session["logged_in"] = True
+    return redirect(url_for("home_page"))
+  else:
+    error_message = "Incorrect PIN. Please try again."
+    return render_template("login.html", error=error_message)
+
+
+@app.route("/index")
+@login_required
+def home_page():
+  return render_template("index.html")
+
+
+@app.route("/footage/full/<cameratype>/<route>")
+@login_required
+def full(cameratype, route):
+  chunk_size = 1024 * 512  # 5KiB
+  file_name = cameratype + (".ts" if cameratype == "qcamera" else ".hevc")
+  vidlist = "|".join(REALDATA + "/" + segment + "/" + file_name for segment in fleet.segments_in_route(route))
+
+  def generate_buffered_stream():
+    with fleet.ffmpeg_mp4_concat_wrap_process_builder(vidlist, cameratype, chunk_size) as process:
+      for chunk in iter(lambda: process.stdout.read(chunk_size), b""):
+        yield bytes(chunk)
+  return Response(generate_buffered_stream(), status=200, mimetype='video/mp4')
+
+
+@app.route("/footage/<cameratype>/<segment>")
+@login_required
+def fcamera(cameratype, segment):
+  if not fleet.is_valid_segment(segment):
+    return render_template("error.html", error="invalid segment")
+  file_name = REALDATA + "/" + segment + "/" + cameratype + (".ts" if cameratype == "qcamera" else ".hevc")
+  return Response(fleet.ffmpeg_mp4_wrap_process_builder(file_name).stdout.read(), status=200, mimetype='video/mp4')
+
+
+@app.route("/footage/<route>")
+@login_required
+def route(route):
+  if len(route) != 20:
+    return render_template("error.html", error="route not found")
+
+  if str(request.query_string) == "b''":
+    query_segment = str("0")
+    query_type = "qcamera"
+  else:
+    query_segment = (str(request.query_string).split(","))[0][2:]
+    query_type = (str(request.query_string).split(","))[1][:-1]
+
+  links = ""
+  segments = ""
+  for segment in fleet.segments_in_route(route):
+    links += "<a href='"+route+"?"+segment.split("--")[2]+","+query_type+"'>"+segment+"</a><br>"
+    segments += "'"+segment+"',"
+  return render_template("route.html", route=route, query_type=query_type, links=links, segments=segments, query_segment=query_segment)
+
+
+@app.route("/footage")
+@login_required
+def footage():
+  return render_template("footage.html", rows=fleet.all_routes())
+
+
+@app.route("/screenrecords")
+@login_required
+def screenrecords():
+  rows = fleet.all_screenrecords()
+  if not rows:
+    return render_template("error.html", error="no screenrecords found at:<br><br>" + fleet.SCREENRECORD_PATH)
+  return render_template("screenrecords.html", rows=rows, clip=rows[0])
+
+
+@app.route("/screenrecords/<clip>")
+@login_required
+def screenrecord(clip):
+  return render_template("screenrecords.html", rows=fleet.all_screenrecords(), clip=clip)
+
+
+@app.route("/screenrecords/play/pipe/<file>")
+@login_required
+def videoscreenrecord(file):
+  file_name = fleet.SCREENRECORD_PATH + file
+  return Response(fleet.ffplay_mp4_wrap_process_builder(file_name).stdout.read(), status=200, mimetype='video/mp4')
+
+
+@app.route("/screenrecords/download/<clip>")
+@login_required
+def download_file(clip):
+  return send_from_directory(fleet.SCREENRECORD_PATH, clip, as_attachment=True)
+
+
+@app.route("/about")
+@login_required
+def about():
+  return render_template("about.html")
+
+
+def main():
+  if not os.path.exists(fleet.PIN_PATH):
+    os.makedirs(fleet.PIN_PATH)
+  pin = str(random.randint(100000, 999999))
+  with open(fleet.PIN_PATH + "otp.conf", "w") as file:
+    file.write(pin)
+
+  app.secret_key = secrets.token_hex(32)
+  app.run(host="0.0.0.0", port=5050)
+
+
+if __name__ == '__main__':
+  main()
